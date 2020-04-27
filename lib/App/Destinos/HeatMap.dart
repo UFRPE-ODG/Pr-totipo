3e4d2a4e31b6c9d7383d273c01a629d89dd0e7e6
Lyly <51674050+Maelyly@users.noreply.github.com>
2020-04-27 16:22:03 -0300

    heatmap com markers

--- lib/App/Destinos/HeatMap.dart
+++ lib/App/Destinos/HeatMap.dart
@@ -2,20 +2,79 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter_heatmap/google_maps_flutter_heatmap.dart';
+bool marcadores = true; 
+var _markers = Set<Marker>();
+LatLng pinPosition = LatLng(34.42796133580664, -100.985749655962); 
+LatLng pinPosition2 = LatLng(34.52796133580664, -100.995749655962); 
+LatLng pinPosition3 = LatLng(34.62796133580664, -101.085749655962);
+LatLng pinPosition4 = LatLng(34.82796133580664, -100.685749655962); 
+LatLng pinPosition5 = LatLng(34.34796133580664, -100.085749655962);  
+addMarkers(){
+  _markers.add(
+     Marker(
+      markerId: MarkerId('aluno'),
+      position: pinPosition,
+      visible: marcadores,
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+
+    )
+  );
+  _markers.add(
+     Marker(
+      markerId: MarkerId('aluno2'),
+      position: pinPosition2,
+      visible: marcadores,
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+
+    )
+  );
+  _markers.add(
+     Marker(
+      markerId: MarkerId('aluno3'),
+      position: pinPosition3,
+      visible: marcadores,
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+
+    )
+  );
+  _markers.add(
+     Marker(
+      markerId: MarkerId('aluno4'),
+      position: pinPosition4,
+      visible: marcadores,
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+
+    )
+  );
+  _markers.add(
+     Marker(
+      markerId: MarkerId('aluno5'),
+      position: pinPosition5,
+      visible: marcadores,
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+
+    )
+  );
+}
 
 class MapSample extends StatefulWidget {
   @override
   State<MapSample> createState() => MapSampleState();
 }
 
+
 class MapSampleState extends State<MapSample> {
   Completer<GoogleMapController> _controller = Completer();
   final Set<Heatmap> _heatmaps = {};
+
   static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
+    target: LatLng(34.42796133580664, -100.985749655962),
+    zoom: 10.47,
   );
+
   LatLng _heatmapLocation = LatLng(37.42796133580664, -122.085749655962);
+
+
   static final CameraPosition _kLake = CameraPosition(
       bearing: 192.8334901395799,
       target: LatLng(37.43296265331129, -122.08832357078792),
@@ -26,12 +85,15 @@ class MapSampleState extends State<MapSample> {
   Widget build(BuildContext context) {
     return new Scaffold(
       body: GoogleMap(
+        markers: _markers,
         mapType: MapType.hybrid,
         initialCameraPosition: _kGooglePlex,
         heatmaps: _heatmaps,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
+          
         },
+        
       ),
       /*floatingActionButton: FloatingActionButton.extended(
         onPressed: _goToTheLake,
@@ -44,13 +106,15 @@ class MapSampleState extends State<MapSample> {
         icon: Icon(Icons.add_box),
       ),
     );
+    
   }
   void _addHeatmap(){
     setState(() {
+      addMarkers();
       _heatmaps.add(
         Heatmap(
           heatmapId: HeatmapId(_heatmapLocation.toString()),
-          points: _createPoints(_heatmapLocation),
+          points:  heatpormarkers(),
           radius: 20,
           visible: true,
           gradient:  HeatmapGradient(
@@ -64,16 +128,20 @@ class MapSampleState extends State<MapSample> {
   List<WeightedLatLng> _createPoints(LatLng location) {
     final List<WeightedLatLng> points = <WeightedLatLng>[];
     //Can create multiple points here
-    points.add(_createWeightedLatLng(location.latitude,location.longitude, 1));
-    points.add(_createWeightedLatLng(location.latitude-1,location.longitude, 1)); 
-    points.add(_createWeightedLatLng(location.latitude+1.4,location.longitude, 1)); 
-    points.add(_createWeightedLatLng(location.latitude+2,location.longitude+1, 1)); 
-    points.add(_createWeightedLatLng(location.latitude+2,location.longitude+0.7, 1)); 
-    points.add(_createWeightedLatLng(location.latitude+25,location.longitude+0.9, 1)); 
-    points.add(_createWeightedLatLng(location.latitude+2,location.longitude, 1)); 
+    points.add(_createWeightedLatLng(34.62796133580664, -100.685749655962, 2));
+    points.add(_createWeightedLatLng(34.52796133580663, -100.985749655962, 3)); 
+    points.add(_createWeightedLatLng(34.43796133580654, -100.885749655963, 2)); 
     return points;
   }
 
+  heatpormarkers(){
+    final List<WeightedLatLng> points2 = <WeightedLatLng>[];
+    for(Marker a in _markers){
+      points2.add(_createWeightedLatLng(a.position.latitude, a.position.longitude, 3));
+    }
+    marcadores = !marcadores;
+    return points2;
+  }
   WeightedLatLng _createWeightedLatLng(double lat, double lng, int weight) {
     return WeightedLatLng(point: LatLng(lat, lng), intensity: weight);
   }
@@ -83,3 +151,4 @@ class MapSampleState extends State<MapSample> {
     controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
+
